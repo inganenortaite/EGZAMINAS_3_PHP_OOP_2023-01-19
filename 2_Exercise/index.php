0fbcb2d16f19f18bc7f3dbec429d13c7adb512dc
inga nenortaite <inga.nenortaite@gmail.com>
2023-01-22 19:28:03 +0200

    EXAM_PHP_3_FINISHED+Number_Format

--- 2_Exercise/index.php
+++ 2_Exercise/index.php
@@ -9,8 +9,8 @@ use Exercise2\StandardPriceCalculator;
 use Exercise2\SubscriberPriceCalculator;
 
 $firstTicket = new CinemaTicket("Shotgun Wedding", "Eilė 6, Vieta 3", new \DateTime ("2023-01-23"), 10);
-$secondTicket = new CinemaTicket("A Man Called Otto", "Eilė 6, Vieta 4", new \DateTime ("2023-01-23"), 14);
-$thirdTicket = new CinemaTicket("Puss in Boots: The Last Wish", "Eilė 6, Vieta 5", new \DateTime ("2023-01-23"), 8);
+$secondTicket = new CinemaTicket("A Man Called Otto", "Eilė 8, Vieta 11", new \DateTime ("2023-01-24"), 14);
+$thirdTicket = new CinemaTicket("Puss in Boots: The Last Wish", "Eilė 9, Vieta 14", new \DateTime ("2023-01-25"), 8);
 
 $standartTicketsCalculator = new StandardPriceCalculator;
 $subscriberTicketsCalculator = new SubscriberPriceCalculator;
@@ -23,14 +23,14 @@ $newCustomerTicketsOrderProcessor = new OrderProcessor($newCustomerTicketsCalcul
 $standartTicketsOrderProcessor->addItem($firstTicket);
 $standartTicketsOrderProcessor->addItem($secondTicket);
 $standartTicketsOrderProcessor->addItem($thirdTicket);
-echo "Standartinio užsakymo kaina yra " . $standartTicketsOrderProcessor->calculatePrice() . " EUR" . PHP_EOL;
+echo "Standartinio užsakymo kaina yra " . number_format($standartTicketsOrderProcessor->calculatePrice(), 2) . " Eur" . PHP_EOL;
 
 $subscriberTicketsOrderProcessor->addItem($firstTicket);
 $subscriberTicketsOrderProcessor->addItem($secondTicket);
 $subscriberTicketsOrderProcessor->addItem($thirdTicket);
-echo "Prenumeratoriaus užsakymo kaina yra " . $subscriberTicketsOrderProcessor->calculatePrice() . " EUR" . PHP_EOL;
+echo "Prenumeratoriaus užsakymo kaina yra " . number_format($subscriberTicketsOrderProcessor->calculatePrice(), 2) . " Eur" . PHP_EOL;
 
 $newCustomerTicketsOrderProcessor->addItem($firstTicket);
 $newCustomerTicketsOrderProcessor->addItem($secondTicket);
 $newCustomerTicketsOrderProcessor->addItem($thirdTicket);
-echo "Naujai prisijungusio pirkėjo užsakymo kaina yra " . $newCustomerTicketsOrderProcessor->calculatePrice() . " EUR" . PHP_EOL;
+echo "Naujai prisijungusio pirkėjo užsakymo kaina yra " . number_format($newCustomerTicketsOrderProcessor->calculatePrice(), 2) . " Eur" . PHP_EOL;
